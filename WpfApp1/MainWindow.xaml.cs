15ceba5ac38ccea7fd9bcfdf003934a3fdd4df11
ComSpex <comspex@ict.jp>
2019-08-11 16:01:58 +0900

    1.3

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -3,6 +3,9 @@ using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
+using System.Timers;
+using TimerTimer = System.Timers.Timer;
+using Timer = System.Threading.Timer;
 
 namespace WpfApp1 {
 	/// <summary>
@@ -10,43 +13,79 @@ namespace WpfApp1 {
 	/// </summary>
 	public partial class MainWindow:Window {
 		Timer tick;
+		TimerTimer tack;
+		const int interval = 10;
 		delegate void ThreadSafe1(object arg);
 		public MainWindow() {
 			InitializeComponent();
-			CreateOneThread(this.face);
+			CreateTimerOne(this.face);
+			//CreateTimerTwo(this.face);
 		}
-		private void CreateOneThread(object arg) {
+		private void CreateTimerTwo(object arg) {
+			if(tack!=null) {
+				TextBlock tb = arg as TextBlock;
+				if(!tb.Dispatcher.CheckAccess()) {
+					try {
+						Dispatcher.Invoke(() => CreateTimerTwo(arg));
+					} catch { }
+				} else {
+					tb.Text=DateTime.Now.ToString("HH:mm:ss.fff");
+				}
+				DoEvents();
+			} else {
+				tack=new TimerTimer();
+				tack.Interval=interval;
+				tack.Elapsed+=Tack_Elapsed;
+				tack.Start();
+			}
+		}
+		private void Tack_Elapsed(object sender,ElapsedEventArgs e) {
+			TextBlock tb = this.face;
+			if(!tb.Dispatcher.CheckAccess()) {
+				try {
+					Dispatcher.Invoke(() => CreateTimerTwo(tb));
+				} catch { }
+			} else {
+				tb.Text=DateTime.Now.ToString("HH:mm:ss.fff");
+			}
+		}
+		private void CreateTimerOne(object arg) {
 			if(tick!=null) {
 				TextBlock tb = arg as TextBlock;
 				if(!tb.Dispatcher.CheckAccess()) {
 					try {
-						Dispatcher.Invoke(() => CreateOneThread(arg));
+						Dispatcher.Invoke(() => CreateTimerOne(arg));
 					} catch { }
 				} else {
 					tb.Text=DateTime.Now.ToString("HH:mm:ss.fff");
 				}
-				//Thread.Sleep(0);
 				DoEvents();
+			} else {
+				tick=new Timer((o) => {
+					TextBlock tb = o as TextBlock;
+					if(!tb.Dispatcher.CheckAccess()) {
+						try {
+							Dispatcher.Invoke(() => CreateTimerOne(arg));
+						} catch { }
+					} else {
+						tb.Text=DateTime.Now.ToString("HH:mm:ss.fff");
+					}
+				},arg,1000,interval);
 			}
-			tick=new Timer((o) => {
-				TextBlock tb = o as TextBlock;
-				if(!tb.Dispatcher.CheckAccess()) {
-					try {
-						Dispatcher.Invoke(() => CreateOneThread(arg));
-					} catch { }
-				} else {
-					tb.Text=DateTime.Now.ToString("HH:mm:ss.fff");
-				}
-			},arg,1000,10);
 		}
 		public void DoEvents() {
 			DispatcherFrame frame = new DispatcherFrame();
 			Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Background,
-				new DispatcherOperationCallback((o)=> {
+				new DispatcherOperationCallback((o) => {
 					((DispatcherFrame)o).Continue=false;
 					return null;
 				}),frame);
 			Dispatcher.PushFrame(frame);
+		}
+		private void Button_Click(object sender,RoutedEventArgs e) {
+			if(MessageBoxResult.OK==MessageBox.Show("Are you sure to terminate this program?",this.Title,MessageBoxButton.OKCancel,MessageBoxImage.Question)) {
+				Close();
+			}
 		}
 	}
 }
