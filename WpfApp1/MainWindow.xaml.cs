2e935539f3240e4b17250dd7c8e8b897a92a55d0
Yosuke Maeda <comspex.w10@outlook.com>
2019-08-11 10:19:36 +0900

    1.2

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -3,6 +3,9 @@ using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Threading;
+using System.Timers;
+using TimerTimer = System.Timers.Timer;
+using Timer = System.Threading.Timer;
 
 namespace WpfApp1 {
 	/// <summary>
@@ -10,17 +13,48 @@ namespace WpfApp1 {
 	/// </summary>
 	public partial class MainWindow:Window {
 		Timer tick;
+		TimerTimer tack;
+		const int interval = 10;
 		delegate void ThreadSafe1(object arg);
 		public MainWindow() {
 			InitializeComponent();
-			CreateOneThread(this.face);
+			//CreateTimerOne(this.face);
+			CreateTimerTwo(this.face);
 		}
-		private void CreateOneThread(object arg) {
+		private void CreateTimerTwo(object arg) {
+			if(tack!=null) {
+				TextBlock tb = arg as TextBlock;
+				if(!tb.Dispatcher.CheckAccess()) {
+					try {
+						Dispatcher.Invoke(() => CreateTimerTwo(arg));
+					} catch { }
+				} else {
+					tb.Text=DateTime.Now.ToString("HH:mm:ss.fff");
+				}
+				DoEvents();
+			} else {
+				tack=new TimerTimer();
+				tack.Interval=interval;
+				tack.Elapsed+=Tack_Elapsed;
+				tack.Start();
+			}
+		}
+		private void Tack_Elapsed(object sender,ElapsedEventArgs e) {
+			TextBlock tb = this.face;
+			if(!tb.Dispatcher.CheckAccess()) {
+				try {
+					Dispatcher.Invoke(() => CreateTimerTwo(tb));
+				} catch { }
+			} else {
+				tb.Text=DateTime.Now.ToString("HH:mm:ss.fff");
+			}
+		}
+		private void CreateTimerOne(object arg) {
 			if(tick!=null) {
 				TextBlock tb = arg as TextBlock;
 				if(!tb.Dispatcher.CheckAccess()) {
 					try {
-						Dispatcher.Invoke(() => CreateOneThread(arg));
+						Dispatcher.Invoke(() => CreateTimerOne(arg));
 					} catch { }
 				} else {
 					tb.Text=DateTime.Now.ToString("HH:mm:ss.fff");
@@ -31,12 +65,12 @@ namespace WpfApp1 {
 				TextBlock tb = o as TextBlock;
 				if(!tb.Dispatcher.CheckAccess()) {
 					try {
-						Dispatcher.Invoke(() => CreateOneThread(arg));
+						Dispatcher.Invoke(() => CreateTimerOne(arg));
 					} catch { }
 				} else {
 					tb.Text=DateTime.Now.ToString("HH:mm:ss.fff");
 				}
-			},arg,1000,1000);
+			},arg,1000,interval);
 		}
 		public void DoEvents() {
 			DispatcherFrame frame = new DispatcherFrame();
